3f1f43fe09364f854da634e2c8dbd4fcd8fa2c09
roopepi <roope.pitkanen@metropolia.fi>
2025-08-27 17:01:31 +0300

    Update tervehdys.py

--- tervehdys.py
+++ tervehdys.py
@@ -1,1 +1,4 @@
+C:\Users\roope.pitkanen\PycharmProjects\PythonProject2\.venv\Scripts\python.exe C:\Users\roope.pitkanen\PycharmProjects\PythonProject2\tervehdys.py 
+Terve, Roope!
 
+Process finished with exit code 0

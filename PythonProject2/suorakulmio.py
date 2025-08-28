9e14d1f259c788a206aa722181ec3dd36d3d781e
roopepi <roope.pitkanen@metropolia.fi>
2025-08-28 15:15:11 +0300

    Suorakulmio.py commit

--- PythonProject2/suorakulmio.py
+++ PythonProject2/suorakulmio.py
@@ -6,6 +6,6 @@ korkeus = float(input("Anna suorakulmion korkeus: "))
 pinta_ala = kanta * korkeus
 piiri = 2 * (kanta + korkeus)
 
-# Tulosta tulokset
+# Anna tulokset
 print(f"Suorakulmion pinta-ala on {pinta_ala:.2f}")
 print(f"Suorakulmion piiri on {piiri:.2f}")

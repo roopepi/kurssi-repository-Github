23a3dda65313a09277d188e4400cb96470fe73c9
roopepi <roope.pitkanen@metropolia.fi>
2025-08-28 15:28:44 +0300

    numerolukko.py random.randint commit

---
+++ PythonProject2/numerolukko.py
@@ -0,0 +1,8 @@
+import random
+
+kolminumeroinen = str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
+nelinumeroinen = str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6))
+
+print("Kolminumeroinen koodi:", kolminumeroinen)
+print("Nelinumeroinen koodi:", nelinumeroinen)
+

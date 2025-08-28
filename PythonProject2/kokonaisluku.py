cc6e0590bbc5a9ffc9f308e0a47f7151e142bc40
roopepi <roope.pitkanen@metropolia.fi>
2025-08-28 15:22:37 +0300

    kokonaisluku.py commit

---
+++ PythonProject2/kokonaisluku.py
@@ -0,0 +1,14 @@
+# Kysy kolme kokonaislukua
+luku1 = int(input("Anna ensimmäinen kokonaisluku: "))
+luku2 = int(input("Anna toinen kokonaisluku: "))
+luku3 = int(input("Anna kolmas kokonaisluku: "))
+
+# Laske summa, tulo ja keskiarvo
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+
+# Tulosta tulokset
+print("Summa:", summa)
+print("Tulo:", tulo)
+print("Keskiarvo:", keskiarvo)

527dba2354e4f7dc807865e4f6648c3ae999b36b
roopepi <roope.pitkanen@metropolia.fi>
2025-09-05 13:22:32 +0300

    karkausvuosi.py

---
+++ karkausvuosi.py
@@ -0,0 +1,11 @@
+# Kysy käyttäjältä vuosiluku
+try:
+    vuosi = int(input("Anna vuosiluku: "))
+
+    # Tarkistetaan karkausvuosi
+    if (vuosi % 4 == 0 and vuosi % 100 != 0) or (vuosi % 400 == 0):
+        print(f"{vuosi} on karkausvuosi.")
+    else:
+        print(f"{vuosi} ei ole karkausvuosi.")
+except ValueError:
+    print("Virheellinen syöte. Anna vuosiluku kokonaislukuna.")

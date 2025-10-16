0156556afd517897eda8522e97db38b095317838
roopepi <roope.pitkanen@metropolia.fi>
2025-10-16 10:55:23 +0300

    kaupunki.py

---
+++ kaupunki.py
@@ -0,0 +1,9 @@
+kaupungit = []
+
+for i in range(5):
+    nimi = input("Anna kaupungin nimi: ")
+    kaupungit.append(nimi)
+
+print("Syötetyt kaupungit:")
+for kaupunki in kaupungit:
+    print(kaupunki)
